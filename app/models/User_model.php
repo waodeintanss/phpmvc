f9eb43f79fd3f3fce2080d6bbcb413f9f4b5e414
Intan <waodeintansss@gmail.com>
2019-10-08 00:23:10 +0800

    membuat model

---
+++ app/models/User_model.php
@@ -0,0 +1,12 @@
+<?php 
+
+class User_model{
+	private $nama = 'Sandhika';
+
+	public function getUser()
+	{
+		return $this->nama; 
+	}
+}
+
+ ?>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -18,6 +18,7 @@
         <a class="nav-link" href="<?= BASEURL; ?>">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
+        <a class="nav-link" href="<?= BASEURL; ?>/mahasiswa">Mahasiswa</a>
         <a class="nav-link" href="<?= BASEURL; ?>/about">About</a>
       </li>
     </ul>
