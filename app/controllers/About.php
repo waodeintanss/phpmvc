46b4a6a93af550e7a89ef2da4249e48aba2be270
Intan <waodeintansss@gmail.com>
2019-10-07 16:36:51 +0800

    membuat controller

---
+++ app/controllers/About.php
@@ -0,0 +1,16 @@
+<?php 
+
+class About {
+	public function index($nama = 'Sandhika', $pekerjaan = 'Dosen')
+	{
+		echo "Halo. nama saya $nama, saya adalah seorang $pekerjaan";
+	}
+	public function page()
+	{
+		echo 'About/page';
+	}
+} 
+
+
+
+ ?> 
